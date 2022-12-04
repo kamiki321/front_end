15ceb6edcca82df35ba372e707ece6bc5e523e91
Rio Bintang Sadewa <82932748+kamiki321@users.noreply.github.com>
2022-12-04 20:14:43 +0700

    Add files via upload

---
+++ src/pages/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
+const NotFound = () => {
+  return (
+    <div><h1 style={{textAlign: "center", color: "purple",marginTop:"20%",fontSize: "100px"}}>PAGE NOT FOUND!!!</h1>
+    <h2 style={{textAlign: "center", color: "purple",marginTop:"30%",fontSize: "70px"}}>Error 404</h2>
+    </div>
+  )
+}
+
+export default NotFound;
